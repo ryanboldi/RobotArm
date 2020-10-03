c5ca110878c59aaf2683b1a92bc4cd16cade4b3a
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-10-03 20:27:26 +0400

    first robot arm kinda works

--- src/arm.js
+++ src/arm.js
@@ -4,19 +4,34 @@ class Arm {
         this.armSegLength = 50;
         this.armSegWidth = 20;
 
-        this.theta1 = 0;
+        this.theta1 = PI / 3;
         this.theta2 = 0;
+
+        this.timeCounter = 0;
     }
 
     draw() {
+        noStroke();
+        fill(0);
+        rectMode(CENTER);
+        rect(WIDTH / 2, HEIGHT / 1.2, 200, 20);
 
+        push();
+        //move to center
+        translate(WIDTH / 2, HEIGHT / 1.2);
+        rotate(this.theta1);
+        translate(-WIDTH / 2, -HEIGHT / 1.2);
+        rect(WIDTH / 2, HEIGHT / 1.2 - 80, 20, 160);
+        pop();
+
+        this.theta1 = this.theta1 + this.getTheta1();
     }
 
-    getDeltaTheta1(time) {
-        return time;
+    getTheta1() {
+        return this.timeCounter / 10;
     }
 
-    getDeltaTheta2(time) {
-        return time;
+    getTheta2() {
+        return this.timeCounter / 10;
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 const WIDTH = 800,
     HEIGHT = 800;
 
+let a;
+
 function setup() {
     createCanvas(WIDTH, HEIGHT);
+    a = new Arm();
 }
 
 function draw() {
     background(210);
+    a.draw();
 }
