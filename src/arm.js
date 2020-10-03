0b6ea21ea744f48c4e8de276e36ead8548a2c69e
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-10-03 21:10:09 +0400

    three articulated segments: creepy

--- src/arm.js
+++ src/arm.js
@@ -1,11 +1,12 @@
 class Arm {
     constructor() {
         this.hinges = 2;
-        this.armSegLength = 160;
+        this.armSegLength = 150;
         this.armSegWidth = 20;
 
         this.theta1 = 0;
         this.theta2 = 0;
+        this.theta3 = 0;
 
 
         this.timeCounter = 0;
@@ -13,7 +14,7 @@ class Arm {
 
     draw() {
         let platformX = WIDTH / 2;
-        let platformY = 600;
+        let platformY = 400;
 
 
         //noStroke();
@@ -29,10 +30,14 @@ class Arm {
         translate(this.armSegLength, 0);
         rotate(this.theta2);
         line(0, 0, this.armSegLength, 0);
+        translate(this.armSegLength, 0);
+        rotate(this.theta3);
+        line(0, 0, this.armSegLength, 0);
         pop();
 
         this.theta1 = this.getTheta1();
         this.theta2 = this.getTheta2();
+        this.theta3 = this.getTheta3();
         this.timeCounter++;
     }
 
@@ -41,6 +46,10 @@ class Arm {
     }
 
     getTheta2() {
+        //return 0;
+        return -this.timeCounter / 100;
+    }
+    getTheta3() {
         return this.timeCounter / 100;
     }
 }
