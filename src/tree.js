f66e6ba514bb15740f8495aa58405b5bb3e6e2be
Ryan Boldi <ryan.boldi123@gmail.com>
2020-10-04 13:18:49 +0400

    Update tree.js

--- src/tree.js
+++ src/tree.js
@@ -3,7 +3,7 @@ class Tree{
         this.functions = ['+','-','/','*'];
         this.semifunctions = ['min', 'max'];
         this.semiterminals = ['sin', 'cos', 'tan', 'cosh', 'sinh', 'tanh'];
-        this.terminals = ['t', this.getRandomEquation()];
+        this.terminals = ['t', this.getRandomEquation(),  this.getRandomEquation(),  this.getRandomEquation()];
 
         this.equation = this.getRandomEquation();
         console.log(this.equation);
@@ -14,9 +14,9 @@ class Tree{
         let ran = random();
         if (ran < 0.3){
             return(`(${random(this.terminals)} ${random(this.functions)} ${random(this.terminals)})`);
-        } else if (ran < 0.3){
+        } else if (ran < 0.5){
             return(`${random(this.semifunctions)}(${random(this.terminals)}, ${random(this.terminals)})`);
-        } else if (ran < 0.95){
+        } else if (ran < 0.75){
             return(`${random(this.semiterminals)}(${random(this.terminals)})`)
         } else{
             return(`${random(this.terminals)}`);
