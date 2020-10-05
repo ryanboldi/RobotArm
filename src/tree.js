e624e51736384028f82d635cba36e498a49b1470
Ryan Boldi <ryan.boldi123@gmail.com>
2020-10-05 12:27:31 +0400

    Update tree.js

--- src/tree.js
+++ src/tree.js
@@ -2,24 +2,30 @@ class Tree{
     constructor(){
         this.functions = ['+','-','/','*'];
         this.semifunctions = ['min', 'max'];
-        this.semiterminals = ['sin', 'cos', 'tan', 'cosh', 'sinh', 'tanh'];
-        this.terminals = ['t', this.getRandomEquation().toString()];
+        this.semiterminals = ['sin', 'cos', 'tan'];
+        this.terminals = ['t', '0.1'];
 
         this.equation = this.getRandomEquation();
+        console.log(this.equation);
         console.log(nerdamer(this.equation).text());
     }
 
-    getRandomEquation(){
+    getRandomEquation(t){
         //pick two random functions, if trig function,
         let ran = random();
-        if (ran < 0.3){
-            return(`(${random(this.terminals)} ${random(this.functions)} ${random(this.terminals)})`);
-        } else if (ran < 0.5){
-            return(`${random(this.semifunctions)}(${random(this.terminals)}, ${random(this.terminals)})`);
-        } else if (ran < 1){
-            return(`${random(this.semiterminals)}(${random(this.terminals)})`)
+        let func = NaN;
+        if (ran < 0.1){
+            func = (`(${random(this.terminals)} ${random(this.functions)} ${random(this.terminals)})`);
+        } else if (ran < 0.2){
+            func = (`${random(this.semifunctions)}(${random(this.terminals)}, ${random(this.terminals)})`);
+        } else if (ran < 0.3){
+            func = (`${random(this.semiterminals)}(${random(this.terminals)})`)
         } else{
-            return(`${random(this.terminals)}`);
+            func = (`(${random(this.terminals)} ${random(this.functions)} ${this.getRandomEquation().toString()})`);
         }
+
+        console.log(func);
+        return func
+        
     }
 }
