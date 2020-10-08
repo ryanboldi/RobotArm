b95708e3cdef6a9676592b274cabc2400458309b
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-10-08 19:18:42 +0400

    spacing

--- src/main.js
+++ src/main.js
@@ -10,17 +10,17 @@ let arms = [];
 let t;
 
 const ArmsPerGen = 8;
-const survivors = ArmsPerGen/2;
+const survivors = ArmsPerGen / 2;
 let generation = 1;
 
 function setup() {
     createCanvas(WIDTH, HEIGHT);
-    for (let i = 0; i< ArmsPerGen; i++){arms.push(new Arm());}
+    for (let i = 0; i < ArmsPerGen; i++) { arms.push(new Arm()); }
 
     background(255);
     fill(200);
     ellipse(WIDTH / 2, HEIGHT / 2, 800, 800);
-    for (let i = 0; i< ArmsPerGen; i++){arms[i].draw();}
+    for (let i = 0; i < ArmsPerGen; i++) { arms[i].draw(); }
 }
 
 function draw() {
@@ -37,7 +37,7 @@ function draw() {
         pop();
     }
 
-    
+
     if (!userDrawing) {
         background(255);
         fill(200);
@@ -55,32 +55,32 @@ function draw() {
         endShape();
         pop();
 
-        
-        
-        for (let i = 0; i< ArmsPerGen; i++){arms[i].draw();}
+
+
+        for (let i = 0; i < ArmsPerGen; i++) { arms[i].draw(); }
         let allDone = true;
-        for (let i = 0; i< ArmsPerGen; i++){
-            if (arms[i].moving){
+        for (let i = 0; i < ArmsPerGen; i++) {
+            if (arms[i].moving) {
                 allDone = false;
             }
         }
 
-        if (allDone){
+        if (allDone) {
             NewGeneration();
             noLoop();
         }
     }
 }
 
-function NewGeneration(){
+function NewGeneration() {
     //when all creatures are done, sort by fitness, pick new generation, clear board, and start again.
     let sortedArms = arms.sort((a, b) => (a.fitness > b.fitness) ? 1 : -1); //sort ascending
     let parentArms = _.cloneDeep(sortedArms).splice(0, survivors);
 
     let childrenArms = [];
     //half children made from crossover
-    if (ArmsPerGen > 1){
-        for (let i = 0; i < floor(ArmsPerGen/2); i++){
+    if (ArmsPerGen > 1) {
+        for (let i = 0; i < floor(ArmsPerGen / 2); i++) {
             let localParents = _.cloneDeep(parentArms);
             let parentIndex = floor(random(localParents.length));
             let parent1 = localParents.splice(parentIndex, 1);
@@ -94,7 +94,7 @@ function NewGeneration(){
             child.theta2Tree = t2;
             childrenArms.push(child);
         }
-        for (let i = childrenArms.length; i < ArmsPerGen; i++){
+        for (let i = childrenArms.length; i < ArmsPerGen; i++) {
             //make children via mutation
             childrenArms.push(_.cloneDeep(random(parentArms)).mutate());
         }
@@ -115,8 +115,8 @@ function mouseReleased() {
 
 
 //goes from each vertex to the next, totalling the distance of the whole path
-function getPathLength(path){
-    if (path.length > 1){
+function getPathLength(path) {
+    if (path.length > 1) {
 
         //starting values
         let x = path[0].x;
@@ -128,31 +128,31 @@ function getPathLength(path){
         let totalLength = 0;
 
         //for every vertex
-        for (let i = 1; i< path.length; i++){
+        for (let i = 1; i < path.length; i++) {
             x1 = path[i].x;
             y1 = path[i].y;
 
-            let segmentLength = Math.sqrt(Math.pow((x-x1),2) + Math.pow((y-y1),2));
+            let segmentLength = Math.sqrt(Math.pow((x - x1), 2) + Math.pow((y - y1), 2));
             totalLength += segmentLength;
 
             x = x1;
             y = y1;
         }
         return totalLength;
-    }   
+    }
     return 0;
 }
 
 //returns the difference between two paths' vertices.
-function getTotalPathDifference(path1, path2){
+function getTotalPathDifference(path1, path2) {
     let difference = 0;
 
     //ASSUME path1.length >= path2.length
-    if (path2.length >= path1.length){
-        for(let i = 0; i < path1.length; i++){
+    if (path2.length >= path1.length) {
+        for (let i = 0; i < path1.length; i++) {
             difference += (Math.sqrt(Math.pow(path1[i].x - path2[i].x, 2) + Math.pow(path1[i].y - path2[i].y, 2)));
         }
-    }else{
+    } else {
         console.log("Path lengths don't match up, try again");
         difference = 10000; // BAD
     }
