d69880dafd043f385275de76be381e377839d94d
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-10-09 22:15:05 +0400

    discrete fréchet works

--- src/main.js
+++ src/main.js
@@ -194,6 +194,40 @@ function getTotalPathDifference(path1, path2) {
     return difference;
 }
 
-function DiscreteFrechet(a, b) {
 
+function distance(p1, p2) {
+    return Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2));
+};
+
+//https://gist.github.com/argelius
+function DiscreteFrechet(a, b) {
+    let C = new Float32Array(a.length * b.length),
+        dim = a.length,
+        i, j;
+
+    C[0] = distance(a[0], b[0]);
+
+    console.log(C);
+
+    for (j = 1; j < dim; j++) {
+        C[j] = Math.max(C[j - 1], distance(a[0], b[j]));
+    }
+
+    for (i = 1; i < dim; i++) {
+        C[i * dim] = Math.max(C[(i - 1) * dim], distance(a[i], b[0]));
+    }
+
+
+
+    for (i = 1; i < dim; i++) {
+        for (j = 1; j < dim; j++) {
+            C[i * dim + j] = Math.max(
+                Math.min(C[(i - 1) * dim + j], C[(i - 1) * dim + j - 1], C[i * dim + j - 1]),
+                distance(a[i], b[j])
+            );
+        }
+    }
+
+    return C[C.length - 1];
 }
+
